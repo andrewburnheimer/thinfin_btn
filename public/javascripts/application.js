92dcc897267477481c0599823ac08df629a1d4ab
Andrew Burnheimer <Andrew.Burnheimer@nbcuni.com>
2017-03-27 21:39:15 +0000

    Add log window with clipboard-copy button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,24 +2,39 @@ var freeBusyTogglerTimerId;
 
 $(window).load(function(){
   $(".noscriptmsg").hide();
+
+  $('.nooklink.disabled').on('click', function(e) { e.preventDefault(); });
+
+  logToWindow("Page visited, log started")
+
   $('#disco').change(function() {
     if($(this).prop('checked')){
-      toggleBusyNooksIntoFreeUpState();
+      allBusyNooksIntoFreeUpState();
 
       freeBusyTogglerTimerId = setTimeout(function myFunction() {
         $('#disco').bootstrapToggle('off');
-        toggleFreeUpNooksIntoBusyState();
+        allFreeUpNooksIntoBusyState();
         }, 5000);
     } else {
       clearTimeout(freeBusyTogglerTimerId);
-      toggleFreeUpNooksIntoBusyState();
+      allFreeUpNooksIntoBusyState();
     }
     });
 
-  $('.nooklink.disabled').on('click', function(e) { e.preventDefault(); });
+  $("#copy-btn").click(function(event) {
+    var copyTextarea = $("#log-window");  
+    copyTextarea.select();
+
+    try {  
+      var successful = document.execCommand('copy');  
+    } catch(err) {  
+      console.log('Oops, unable to copy'); 
+    }
+  });
+
 });
 
-function toggleBusyNooksIntoFreeUpState() {
+function allBusyNooksIntoFreeUpState() {
   $.each($('.nooks.busy'), function(index, nook_btn) {
     $(nook_btn).removeClass('disabled')
     $(nook_btn).prop("title", "");
@@ -28,7 +43,7 @@ function toggleBusyNooksIntoFreeUpState() {
   })
 };
 
-function toggleFreeUpNooksIntoBusyState() {
+function allFreeUpNooksIntoBusyState() {
   $.each($('.nooks.busy'), function(index, nook_btn) {
     var nook_btn_span = $(nook_btn).children("span");
     var existing = $(nook_btn_span).text();
@@ -38,3 +53,28 @@ function toggleFreeUpNooksIntoBusyState() {
     $(nook_btn).prop("title", "In-use");
   })
 };
+
+function logToWindow(msg){
+  var d = new Date()
+  var offset = timezoneOffsetToHoursMinutes(d.getTimezoneOffset())
+  $('#log-window').append(d.toLocaleString() + ' ' + offset + ' - ' + msg + '\n')
+  scrollToBottom();
+}
+
+function timezoneOffsetToHoursMinutes(offset){
+  var hours = offset / 60;
+  var minutes = offset % 60;
+
+  return '-' + pad(hours, 2) + ':' + pad(minutes, 2)
+}
+
+/* http://stackoverflow.com/questions/10073699/pad-a-number-with-leading-zeros-in-javascript#answer-10073788 */
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function scrollToBottom() {
+  $('#log-window').scrollTop($('#log-window')[0].scrollHeight);
+}
