12b61941a87b967235e1ae0ef0bbdb1f7707bc86
Andrew Burnheimer <Andrew.Burnheimer@nbcuni.com>
2017-03-28 23:09:59 +0000

    Implement API on server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 var url = require("url");
 var pug = require("pug");
 const querystring = require('querystring');
+var unirest = require('unirest');
 
 var typeHdrForFileExt = function(pathname){
   fileExtRegExp = RegExp("\\.[^.]+$");
@@ -65,16 +66,81 @@ http.createServer(function(request, response) {
     if(pathname == "/roomStatus.json"){
       var query = querystring.parse(reqUrl.query);
       result = new Object
-      result.room = query.room;
-      result.status = "unknown"; /* XXX Implement API here:  */
-      response.statusCode = 200;
-      response.setHeader("Content-Type", "application/json");
-      response.end(JSON.stringify(result));
+
+      if(method == "GET") {
+        if(query.hasOwnProperty("room")) {
+
+          result.room = query.room;
+
+          var facility = new Object;
+          fs.readFile("facilities.json", function (err, content) {
+            array = eval("(" + content + ")");
+            
+            for (var i = 0, len = array.length; i < len; i++) {
+              if(array[i].name == result.room) {
+                facility = array[i];
+                break;
+              }
+            }
+            if(facility.hasOwnProperty("host")){
+
+              unirest.get("http://" + facility.host + ":" + facility.sentryPort + "/NumOfRemoteAppUsers")
+              .end(function (sentryResponse) {
+                if(sentryResponse.ok) {
+                  response.statusCode = 200;
+                  
+                  if(sentryResponse.body.NumOfRemoteAppUsers > 0) {
+                    result.status = "busy";
+                  } else {
+                    result.status = "available";
+
+                  }
+
+                } else {
+                  if(sentryResponse.error.hasOwnProperty("code")) {
+                    response.statusCode = 200;
+                    result.status = "unknown";
+                    console.log("Can't connect to " + query.room + " StudioSentry: " + sentryResponse.error.code);
+                  } else {
+                    response.statusCode = 500;
+                    result.status = "INTERNAL SERVER ERROR";
+                    console.log("ERROR 500, " + query.room + " StudioSentry returning: HTTP " + sentryResponse.statusCode);
+                  }
+                }
+
+                response.setHeader("Content-Type", "application/json");
+                response.end(JSON.stringify(result));
+              });
+            } else {
+              response.statusCode = 404;
+              result.status = "RESOURCE NOT FOUND";
+              console.log("ERROR 404, resource " + result.room + " not found in facilities.json");
+              response.setHeader("Content-Type", "application/json");
+              response.end(JSON.stringify(result));
+            }
+          });
+
+        } else {
+          response.statusCode = 400;
+          result.status = "BAD REQUEST NO PARAMS";
+          console.log("ERROR 400, no query params sent for " + pathname);
+          response.setHeader("Content-Type", "application/json");
+          response.end(JSON.stringify(result));
+        }
+      } else {
+        response.statusCode = 405;
+        result.status = "METHOD NOT ALLOWED";
+        console.log("ERROR 405, " + method + " not allowed for " + pathname);
+        response.setHeader("Content-Type", "application/json");
+        response.end(JSON.stringify(result));
+      }
     } else {
       response.statusCode = 404;
-      response.setHeader('Content-Type', 'text/html');
-      errorResponse = notFoundErrorResponse("Endpoint");
-      response.end(errorResponse());
+      result = new Object
+      result.status = "ENDPOINT NOT FOUND";
+      console.log("ERROR 404, no endpoint for " + pathname);
+      response.setHeader("Content-Type", "application/json");
+      response.end(JSON.stringify(result));
     }
 
   } else if(fileExtRegExp.test(pathname)){
@@ -151,4 +217,3 @@ function otherServerErrorResponse(){
   templateData += "  h2= details";
   return pug.compile(templateData);
 };
-
